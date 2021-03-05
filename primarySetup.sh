aeba14e41885ac899d780b190140cbd2b17d2b02
Bokai Li <Bokai247@gmail.com>
2021-03-05 12:47:31 -0500

    add primary/replica setup script

--- primarySetup.sh
+++ primarySetup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-while getopts "c:d:pk:rk:a:" opt; do
+while getopts "c:b:i:j:k:l:a:" opt; do
   case $opt in
     c) cluster=$OPTARG;;
-    d) deployment=$OPTARG;;
-    pk) primaryKey=$OPTARG;;
-    rk) replicaKey=$OPTARG;;
+    b) DBbinding=$OPTARG;;
+    i) primary=$OPTARG;;
+    k) primaryKey=$OPTARG;;
     a) appName=$OPTARG;;
     *) echo 'invalid flag' >&2
        exit 1
@@ -14,24 +14,42 @@ if [ "$cluster" == "" ]; then
     echo 'Option -c <cluster name/id> is missing' >&2
     exit 1
 fi
-if [ "$deployment" == "" ]; then
-    echo 'Option -d <deployment> is missing' >&2
+if [ "$DBbinding" == "" ]; then
+    echo 'Option -b <binding secret name> is missing' >&2
+    exit 1
+fi
+if [ "$primary" == "" ]; then
+    echo 'Option -i <primary DB instance name> is missing' >&2
     exit 1
 fi
 if [ "$primaryKey" == "" ]; then
-    echo 'Option -p <primary service key name> is missing' >&2
-    exit 1
-fi
-if [ "$replicaKey" == "" ]; then
-    echo 'Option -r <replica service key name> is missing' >&2
+    echo 'Option -k <primary service key name> is missing' >&2
     exit 1
 fi
 if [ "$appName" == "" ]; then
     echo 'Option -a <app name> is missing' >&2
     exit 1
 fi
-# create primary primary service key
+
+#set cluster config
+ibmcloud ks cluster config --cluster $cluster
+kubectl config current-context
+
+# create primary service key
+ibmcloud resource service-key-create $primaryKey --instance-name $primaryKey
 
 # connect to db and restart pods
-
-# create replica instance and key
+#retrieve service key from DB instance
+serviceKey=$(ibmcloud resource service-key $primaryKey --output json)
+# remove '[' and ']'
+echo "${serviceKey:1:${#serviceKey}-2}" > jsonObject.json
+# parse json credentials
+credentials=$(jq '.credentials' jsonObject.json)
+#encode credentials with base64
+encodedString=$(echo $credentials | base64)
+#remove output file
+rm jsonObject.json
+#replace old DB instance binding (failed primary) with new
+kubectl patch secret $DBbinding -p='{"data":{"binding":"'$encodedString'"}}'
+#restart pods
+kubectl rollout restart deployment $appName
---
+++ replicaSetup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+while getopts "c:b:i:j:k:l:a:" opt; do
+  case $opt in
+    k) primaryKey=$OPTARG;;
+    n) replicaName=$OPTARG;;
+    r) region=$OPTARG;;
+    m) memoryAllocation=$OPTARG;;
+    d) diskAllocation=$OPTARG;;
+    l) replicaKey=$OPTARG;;
+    *) echo 'invalid flag' >&2
+       exit 1
+  esac
+done
+if [ "$primaryKey" == "" ]; then
+    echo 'Option -j <primary key name> is missing' >&2
+    exit 1
+fi
+if [ "$replicaName" == "" ]; then
+    echo 'Option -n <replica instance name> is missing' >&2
+    exit 1
+fi
+if [ "$region" == "" ]; then
+    echo 'Option -r <region> is missing' >&2
+    exit 1
+fi
+if [ "$replicaKey" == "" ]; then
+    echo 'Option -l <replica key name> is missing' >&2
+    exit 1
+fi
+
+#retrieve service key from DB instance
+serviceKey=$(ibmcloud resource service-key $primaryKey --output json)
+# remove '[' and ']'
+echo "${serviceKey:1:${#serviceKey}-2}" > jsonObject.json
+# parse json credentials
+crnID=$(jq '.credentials.instance_administration_api.instance_id' jsonObject.json)
+rm jsonObject.json
+# create replica
+ibmcloud resource service-instance-create $replicaName databases-for-postgresql standard $region \
+-p \ '{
+  "remote_leader_id": '$crnID',
+  "members_memory_allocation_mb": "2048",
+  "members_disk_allocation_mb": "10240"
+}'
+
+# create primary service key
+ibmcloud resource service-key-create $replicaKey --instance-name $replicaName
